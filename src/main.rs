6757781ef9fe301e2ab15b1222d105c2b4abecae
Thomas Legris <legris.thomas@gmail.com>
2019-05-02 10:58:25 +0100

    add files

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+extern crate regex;
+use regex::Regex;
+use std::env;
+
+fn main() {
+    println!("Starting email-checker...");
+
+    // Create an email regular expression
+    let re = Regex::new(r"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$").unwrap();
+
+    // Match the first argument against the regular expression
+    match env::args().nth(1) {
+        // We have an argument
+        Some(email) => {
+            if re.is_match(&email) {
+                println!("{} is a valid email.", email);
+            } else {
+                println!("{} is NOT a valid email.", email);
+            }
+        }
+        // No argument provided
+        None => {
+            println!("Please provide a string to test.");
+            return;
+        }
+    };
+};
