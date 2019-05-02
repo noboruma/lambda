7d3d98648eae200ebf56dfd9c39bb0d29aebfe42
Thomas Legris <legris.thomas@gmail.com>
2019-05-02 11:26:21 +0100

    add js wrapper

---
+++ pack/main.js
@@ -0,0 +1,17 @@
+var child_process = require('child_process');
+
+exports.handler = function(event, context) {
+    // event is the JSON we provide to our lambda function. More on this later.
+    console.log(event["email"]);
+
+    // spawn a child process with our email-checker-linux binary and the event["email"] value for our argument.
+    var proc = child_process.spawn('./target/release/email-checker-linux', [ event["email"] ], { stdio: 'inherit' });
+
+    proc.on('close', function(code) {
+        if(code !== 0) {
+            return context.done(new Error("Process exited with non-zero status code"));
+        }
+
+        context.done(null);
+    });
+}
